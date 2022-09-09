1c857baacc6d4c6836ccf12bf66e8d32df13131e
Chad Goh <Chadgoh1993@gmail.com>
2022-09-09 22:52:51 +0800

    added comment

--- app/routes/teacher.routes.js
+++ app/routes/teacher.routes.js
@@ -3,14 +3,13 @@ module.exports = (app) => {
   var router = require("express").Router();
 
   router.post("/teachers/create", teacher.createTeacher);
-
   router.get("/teachers/", teacher.findAll);
   router.post("/teachers/findTeacherById", teacher.findTeacherById);
   router.put("/teachers/updateTeacher", teacher.updateTeacher);
 
   router.post("/register", teacher.registerStudent); // return 204 on success
   router.get("/commonstudents", teacher.commonStudents); // 200 on success
-  router.post("/suspend", teacher.suspendStudent);
+  router.post("/suspend", teacher.suspendStudent); // 204 on success
   router.post("/unsuspend", teacher.unsuspendStudent); // 204 on success
   router.post("/retrievefornotifications"); //200 on success
 
